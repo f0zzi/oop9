3ed195cdb2c1c491258ad3f43ac59c6aef95feae
Alexander Goncharuk <sashael@ukr.net>
2019-08-01 22:35:07 +0300

    Task3 checkpoint

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -3,57 +3,65 @@
 #include <string>
 using namespace std;
 
+bool GetArgs(string& f1, string& f2, int argNumber);
+
 int main()
 {
 	string command, filename1, filename2;
 	char symbol;
 	//enum commands { create, print, append, copy, filterDigits };
+	cout << "Command promt. Waiting for command." << endl;
 	while (true)
 	{
-		//system("cls");
 		command = filename1 = filename2 = "";
-		cout << "Command promt. Waiting for command." << endl << ">";
+		cout << ">";
 		cin >> command;
 		if (command == "create")
 		{
 			cout << "Enter file data:" << endl;
-			cin >> command;
-			cin.ignore();
+			cin >> filename1;
+			while (cin.get() != '\n')
+				continue;
 
-			ofstream fout(command);
+			ofstream fout(filename1);
 			if (fout.is_open())
 			{
 				while (cin.get(symbol))
 					fout.put(symbol);
 				cin.clear();
 				fout.close();
-				//cin.ignore('\n');
 			}
 			else
-				cout << "Error opening file." << endl;
+				cerr << "Error creating file." << endl;
 		}
 		else if (command == "print")
 		{
-			cin >> command;
-			ifstream fin(command);
+			cin >> filename1;
+			ifstream fin(filename1);
 			if (fin.is_open())
 				cout << fin.rdbuf();
+			else
+				cerr << "Error opening file." << endl;
 		}
 		else if (command == "append")
 		{
-			cin >> command;
-			ofstream fout(command, ios::app);
+			cin >> filename1;
+			while (cin.get() != '\n')
+				continue;
+			ofstream fout(filename1, ios::app);
 			if (fout.is_open())
 			{
+				ifstream fin(filename1);
+				cout << fin.rdbuf();
+				fin.close();
 				char symbol;
 				while (cin.get(symbol))
-					//fout.put(symbol);
 					fout << symbol;
 				cin.clear();
 				fout.close();
 			}
 			else
-				cout << "Error opening file." << endl;
+				cerr << "Error opening file." << endl;
 		}
 		else if (command == "copy")
 		{
@@ -71,12 +79,17 @@ int main()
 			cin >> filename1;
 			cin >> filename2;
 			ifstream fin(filename1);
-			ofstream fout(filename2);
-			while (fin.get(symbol))
-				if (!isdigit(symbol))
-					fout << symbol;
-			fin.close();
-			fout.close();
+			if (fin.is_open())
+			{
+				ofstream fout(filename2);
+				while (fin.get(symbol))
+					if (!isdigit(symbol))
+						fout << symbol;
+				fin.close();
+				fout.close();
+			}
+			else
+				cerr << "Error opening file." << endl;
 		}
 		else if (command == "exit" || command == "quit")
 		{
@@ -86,9 +99,27 @@ int main()
 		else
 		{
 			cin.ignore(255, '\n');
-			cout << "Unknown command \"" << command << "\". Try again." << endl;
+			cerr << "Unknown command \"" << command << "\". Try again." << endl;
 		}
+		cout << endl;
 	}
 	system("pause");
 	return 0;
+}
+
+bool GetArgs(string& f1, string& f2, int argNumber)
+{
+	string* tmp[2] = { &f1, &f2 };
+	for (int i = 0; i < argNumber; i++)
+	{
+		cin >> *tmp[i];
+		if (tmp[i]->size() == 0)
+			return false;
+	}
+	if (cin.get() != '\n')
+	{
+		cerr << "Wrong argument number." << endl;
+		return false;
+	}
+	return true;
 }
